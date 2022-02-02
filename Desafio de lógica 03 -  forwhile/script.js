894ab4c9c47e1882ef0c7133199ccd90e817c570
eric Cardoso <eric.alves.cardoso82@gmail.com>
2022-02-02 16:33:36 -0300

    correção

--- Desafio de lógica 03 -  forwhile/script.js
+++ Desafio de lógica 03 -  forwhile/script.js
@@ -1,55 +1,56 @@
-//1 - Criar um loop que conte de 1 até 10 usando FOR
+// //1 - Criar um loop que conte de 1 até 10 usando FOR
 
- for(i = 1; i <= 10; i++ ){
+  for(i = 1; i <= 10; i++ ){
      console.log(i)
  }
 
 
 
 //2 - Criar um loop que conte de 10 até 1 usando WHILE
- let cont = 1
- while(10 >= cont){
+ let cont = 10
+ while(cont >= 1){
      console.log(cont)
-    cont++
+    cont--
  }
 
-//3 - Criar um loop que conte todos os números ímpares de 1 até 100
 
-for(i = 1; i <= 100; i+=2){
-    console.log(i)
-}
+// //3 - Criar um loop que conte todos os números ímpares de 1 até 100
 
-//Com While 
-
-while(100 > cont){
-    if(cont % 2 == 1){
-      console.log(cont)
+ for(i = 1; i <= 100; i+=2){
+     console.log(i)
+ }
+let a = 1 
+ //Com While 
+ while(100 > a){
+    if(a % 2 == 1){
+      console.log(a)
       
     }
-    cont ++
+    a ++
     // ou
 
     // console.log(cont)
     // cont +=2
 
 }
-
+   
 //4- Criar um loop que conte todos os números pares de 0 a 100
 
-for(i = 2; i <= 100; i+=2){
-    console.log(i)
-}
+ for(i = 2; i <= 100; i+=2){
+     console.log(i)
+ }
 
-//com while
+ //com while
 
-let n = 2
+ let n = 2
 
-while(100 >= n){
-    if(n % 2 == 0){
+ while(100 >= n){
+     if(n % 2 == 0){
         console.log(n)
       
     }
     
-    n ++
+   n ++
      
-  }
+}
+ 
